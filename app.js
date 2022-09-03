bd692b26c8e4b4ed76b0baa0a027ed3ad800a9bd
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-09-03 13:50:08 +0600

    responsive

--- app.js
+++ app.js
@@ -36,14 +36,14 @@ const showNews = async (news, name) => {
                 <div class="row">
                 <div class="col-12 mt-4 rounded p-3" style="background-color:white ; overflow:hidden;">
                     <div class="row">
-                        <div class="col-3">
+                        <div class="col-12 col-lg-3 col-md-3 text-center ">
                             <img class ="img-fluid" src="${news.thumbnail_url}" alt="${news.title}" srcset="">
                         </div>
-                        <div class="col-9">
-                            <h4 class = "fw-bold">${news.title.slice(0, 60)}..</h4>
+                        <div class="col-12 col-md-9 col-lg-9">
+                            <h4 class = "fw-bold mt-4 mt-md-0">${news.title.slice(0, 60)}..</h4>
                             <p style = "color: #949494; font-size: 15px;" class ="mt-3">${news.details.slice(0, 700)}.....</p>
                             <div class="row mt-4">
-                                <div class="col-4">
+                                <div class="col-4 col-md-4">
                                     <div class="row">
                                         <div class="col-4">
                                             <img class ="img-fluid rounded-circle" src="${news.author.img ? news.author.img : 'No found img'}" alt="">
@@ -54,14 +54,14 @@ const showNews = async (news, name) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-3">
+                                <div class="col-3 col-md-3">
                                 <i class="fa-solid fa-eye me-2"></i>
                                 <span>${news.total_view}</span>
                                 </div>
-                                <div class="col-3">
+                                <div class="col-3 col-md-3">
                                     <p>Rating: ${news.rating.number}</p>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-2 col-md-2">
                                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showModal('${news._id}')">
                                      <i class="fa-solid fa-arrow-right"></i>
                                 </button>
@@ -109,7 +109,7 @@ const showModal = (newsId) => {
                 <div class="img mt-3">
                   <img class ="img-fluid" src="${modal.image_url}" alt="">
                 </div>
-                <p class ="mt-2 fw-bold">${modal.author.published_date}</p>
+                <p class ="mt-2 fw-bold">Published Date: ${modal.author.published_date.slice(0,10)}</p>
                 <div class="text">
                   <p style="color:#858585" class ="mt-3">${modal.details}</p>
                 </div>
